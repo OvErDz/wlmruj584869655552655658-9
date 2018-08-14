8ae553456679e1926c797475368bd7fe5a1611ce
OvErDz <41547554+OvErDz@users.noreply.github.com>
2018-08-14 15:22:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,7 +88,7 @@ client.on('guildMemberAdd', member => {
     const ei = invites[member.guild.id];
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const yumz = member.guild.channels.find("name", "welcome");
+    const client = member.guild.channels.find("name", "welcome");
      yumz.send(`<@${member.user.id}> **joined by** <@${inviter.id}>`);
   
   }); 
